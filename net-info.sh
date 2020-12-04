9be4f08932c136623a24ff77117db9c490745ecd
root <root@netinfo@wyattkinkade.com>
2020-12-03 23:14:39 -0500

    install.sh

---
+++ net-info.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sleep 75s
+
+CDPINFO=`/usr/sbin/lldpctl`
+
+EXTERNALIP=`curl ifconfig.co`
+
+INTERNALIP=`ip addr | grep eth0 -A 3`
+
+DHCPSVR=`nmap --script broadcast-dhcp-discover -e eth0`
+
+echo "Current External IP address is $EXTERNALIP
+
+LLDP Info is 
+
+$CDPINFO
+
+Internal IP Configuration is 
+
+$INTERNALIP
+
+Further DHCP Configuration information is
+
+$DHCPSVR
+
+" | mutt -s "Network Configuration" wyatt@wyattkinkade.com 
